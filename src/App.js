ca1f444fee90de5f4f252feabd4ccd433b87dca4
wiem <wiemgmc2021@gmail.com>
2021-03-17 00:01:46 +0100

    jsx checkpoint

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import image from './imageInSrc.jpg'
+function App() {
+return (
+<div style={{border:'solid 1px black', maxWidth:'100vw'}}>
+<h1 className="title red">Your name here</h1>
+<br/>
+<img src={image} />
+<br/>
+<img src="/imageInPublic.jpg" />
+<br/>
+ <video  controls width="320" height="240" >
+<source src="myVideo.mp4" type="video/mp4" />
+</video>
+</div>
+  );
+}
+export default App;
